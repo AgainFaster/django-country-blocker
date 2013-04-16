c4eb6a48cea3a16dc2195a1c08aac57d13443d06
Matthew Ghantous <matthew@againfaster.com>
2013-04-16 09:44:27 -0400

    Bumping version to 1.1.2 and adding changelog entry.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ CLASSIFIERS = [
 
 setup(
     name='django-country-blocker',
-    version='1.1.1',
+    version='1.1.2',
     packages=['country_block'],
     url='http://www.github.com/jslootbeek/django-country-blocker',
     license='BSD License',
